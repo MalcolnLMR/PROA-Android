cf45f0f7893807f4f76c88c815d70b835965a11b
MalcolnLMR <malcolnlucas@gmail.com>
2024-10-23 19:26:47 -0300

    update JPCT activity

--- JetPackComposeTutorial/app/src/main/java/br/com/astradev/main/MainActivity.kt
+++ JetPackComposeTutorial/app/src/main/java/br/com/astradev/main/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.astradev.main.dataclasses.Message
 import br.com.astradev.main.ui.theme.JetPackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +26,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetPackComposeTutorialTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Greeting(Message("Martha", "AMERICA YA!"))
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(msg: Message) {
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.download__1_),
+            contentDescription = "Lua",
+            modifier = Modifier
+                .size(40.dp)
+                .padding(4.dp)
+                .clip(CircleShape)
+        )
+
+        Column {
+            Text(
+                text = msg.author
+            )
+            Text(
+                text = msg.body
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetPackComposeTutorialTheme {
-        Greeting("Android")
+        Greeting(Message("Martha", "AMERICA YA!"))
     }
 }
---
+++ JetPackComposeTutorial/app/src/main/java/br/com/astradev/main/dataclasses/Message.kt
@@ -0,0 +1,6 @@
+package br.com.astradev.main.dataclasses
+
+data class Message(
+    val author: String,
+    val body: String
+)
